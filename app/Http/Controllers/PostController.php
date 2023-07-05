7f977d5400bcc65557efbc83e9fe0925373d64c7
Soro <soroferelaha01@gmail.com>
2023-07-05 19:46:06 +0100

    update de la page d'acceuil

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index(){
-        return view('posts.index');
+        // $posts = Post::all(); recupere tout les posts dans l'ordre croissant de leur Id
+        $posts = Post::latest()->get(); // pour recuperer dans l'ordre du plus recent post au moins recent
+
+        //pour transmettre des données a notre vue (dans notre cas present les posts de notre base de donnee) on les passe en deuxieme argument de la function view
+        return view('posts.index', [
+
+            //ceci nous permet de recuperer une variable posts qu'on pourra utiliser dans notre html pour faire une boucle et afficher nos posts
+            'posts' => $posts,
+        ]);
     }
 }
